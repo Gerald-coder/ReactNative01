afb307e3c84399088b731bf053159fe4d4431741
Oduigwe Gerald <paulsongerald00@gmail.com>
2024-03-25 23:22:07 +0100

    working on password generator

--- PassWord.tsx
+++ PassWord.tsx
@@ -1,5 +1,5 @@
 import { View, Text, SafeAreaView } from "react-native";
-import React from "react";
+import React, { useState } from "react";
 import * as Yup from "yup";
 
 export const passwordSchema = Yup.object().shape({
@@ -10,6 +10,60 @@ export const passwordSchema = Yup.object().shape({
 });
 
 const PassWord = () => {
+  const [password, setPassword] = useState("");
+  const [isPasswordGenerated, setIsPasswordGenerated] = useState(false);
+
+  const [lowerCase, setLowerCase] = useState(true);
+  const [upperCase, setUpperCase] = useState(false);
+  const [numbers, setNumbers] = useState(false);
+  const [symbols, setSymbols] = useState(false);
+
+  const generatePasswordString = (passwordLen: number) => {
+    let characterList = "";
+    const uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const lowercase = "abcdefghijklmnopqrstuvwxyz";
+    const numbersChar = "0123456789";
+    const symbolsChar = "!@#$%^&*()_+";
+    if (lowerCase) {
+      characterList += lowercase;
+    }
+    if (upperCase) {
+      characterList += uppercase;
+    }
+    if (numbers) {
+      characterList += numbersChar;
+    }
+    if (symbols) {
+      characterList += symbolsChar;
+    }
+    const passwordResult = createPassword(characterList, passwordLen);
+    setPassword(passwordResult);
+    setIsPasswordGenerated(true);
+    //
+  };
+
+  const createPassword = (characters: string, passwordLen: number) => {
+    let result = "";
+    for (let i = 0; i < passwordLen; i++) {
+      const characterInd = Math.round(Math.random() * characters.length);
+      result += characters.charAt(characterInd);
+    }
+    return result;
+  };
+
+  const resetPassword = () => {
+    setPassword("");
+    setLowerCase(true);
+    setUpperCase(false);
+    setNumbers(false);
+    setSymbols(false);
+    setIsPasswordGenerated(false);
+
+    //
+  };
+
+  // NEXT FORMIK
+
   return (
     <SafeAreaView>
       <Text>PassWord</Text>
